8587f21a7d6344cf8f6f6f7234d73043692a0630
Amrita Arora <amritaarora2424@gmail.com>
2019-09-25 19:49:22 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 
+
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || '6000'; //Checking PORT 
+const port =process.env.PORT || 5000;  //Checking PORT 
 
-//**dataBase_Connectiion**//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//**dataBase_Connection**//
 mongoose.connect("mongodb+srv://ARP_Tool:GU1UbKIVxO9vtNjH@cluster0-etshk.mongodb.net/test?retryWrites=true&w=majority"
 ,{ useNewUrlParser: true})
 .then(() => {
@@ -17,27 +23,18 @@ mongoose.connect("mongodb+srv://ARP_Tool:GU1UbKIVxO9vtNjH@cluster0-etshk.mongodb
   console.log("connection failed");
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
-// app.use(express.json());
 
-// app.use((req,res,next)=>{
-//      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//      res.setHeader("Access-Control-Allow-Headers", "Origin,X-requested-With,Content-Type,application/json,Accept,Authorization");
-//      res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,PUT,DELETE,OPTIONS");
-//  });
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.listen(port ,() => {
+ console.log(`port is running on the port:${port}`);
+});
 
-
-app.get('/',(req,res)=>{
-  console.log("It is Working");
-  res.send("THIS IS EXPRESS");
-})
-
-app.listen(port,()=>{
-  console.log(`port is running on the port:${port}`);
-  console.log("WORKING")
-
-})
-module.exports = app;
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
