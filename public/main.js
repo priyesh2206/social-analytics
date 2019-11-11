58845cfef2f3f9d5ca3550bc0aed4970b8ed6e61
Priyesh <you@example.com>
2019-11-11 21:09:11 +0530

    background

--- public/main.js
+++ public/main.js
@@ -1,43 +1,43 @@
+//userFlag=localStorage.getItem('isLoggedIn');
 
-
+//if(userFlag == true)
+chrome.app.runtime.onLaunched.addListener(()=>{
+  console.log(localStorage.getItem('isLoggedIn'))
+})
 //***********//BACKGROUND PAGE OF CHRONME EXTESION//*****************//
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('onInstalled extension ...');
-    //*****/create alarm after extension is installed / upgrade/*****//
-    chrome.alarms.create('refreshing',{periodInMinutes: 1});
-  });
-  
-chrome.alarms.onAlarm.addListener((alarm) => {
-    console.log(alarm.name);
-    //url_Scraper();
-    //Timer();
-    getAge();
-  });
+  console.log('onInstalled extension ...');
+  //*****/create alarm after extension is installed / upgrade/*****//
+  chrome.alarms.create('refreshing',{periodInMinutes: 1});
+});
 
+chrome.alarms.onAlarm.addListener((alarm) => {
+  console.log(alarm.name);
+  url_Scraper();
+  getAge();
+});
 //*****/scrapes the URl of active tab of user/*****//
 function url_Scraper() {
-    chrome.tabs.query({"active":true,"lastFocusedWindow":true},function(tabs){
-        tabURL = tabs[0].url;
-        console.log("urls",tabURL);
-      })
-  }
-
+  chrome.tabs.query({"active":true,"lastFocusedWindow":true},function(tabs){
+      tabURL = tabs[0].url;
+      console.log("urls",tabURL);
+    })
+}
 //*****/TIMER for user/*****//
 function Timer(){
-    console.log("taburl=",tabURL);
-    for( var i=0 ; i<10 ; i++){
-    urls=['facebook','whatsapp','instagram','twitter','reddit','flickr','wechat','hike','snapchat','telegram'];
-        if(tabURL.includes(urls[i]))
-             {
-                start_Timmer();
-                break;
-              }
-             else{
-               console.log("THE site is changes");
-             } 
-      }
+  console.log("taburl=",tabURL);
+  for( var i=0 ; i<10 ; i++){
+  urls=['facebook','whatsapp','instagram','twitter','reddit','flickr','wechat','hike','snapchat','telegram'];
+      if(tabURL.includes(urls[i]))
+           {
+              start_Timmer();
+              break;
+            }
+           else{
+             console.log("THE site is changes");
+           } 
+    }
 }
-
 //*****/calcute the time/*****//
 var hours,minutes;
 hours=0;
@@ -57,11 +57,24 @@ function start_Timmer(){
    t = setTimeout(time_Cal,1000);
  }
 
+ function stop_timmer(){
+    clearTimeout(t);
+ }
+
  const getAge=()=>{
   console.log(localStorage.getItem('userName'))
   if(localStorage.getItem('userName')!=null)
-  fetch(`http://localhost:4000/api/users/age/${localStorage.getItem('userName')}`).then(res=>res.json()).then(data=>console.log(data))
+  fetch(`http://localhost:4000/api/users/age/${localStorage.getItem('userName')}`).then(res=>res.json()).then(data=>ageTimer(data.Age));
+}
 
- }
- 
- 
+const ageTimer=(age)=>{
+  if(age >=25 || age <=36){
+    console.log("I am Between 11-16");
+    Timer();
+    if(hours>=2){
+      stop_timmer()
+
+    }
+
+  }
+}
